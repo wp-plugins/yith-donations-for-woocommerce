49d8a7960ae1cbe60aef3838536a88160be023d2
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-06 16:09:00 +0000

    fix premium.php
    git-svn-id: https://plugins.svn.wordpress.org/yith-donations-for-woocommerce/trunk@1214172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/add-donation-form-widget.php
+++ templates/add-donation-form-widget.php
@@ -5,12 +5,12 @@ if( !defined( 'ABSPATH' ) )
 <div class="ywcds_form_container">
     <form id="ywcds_add_donation_form" method="get">
         <div class="ywcds_amount_field">
-            <label for="ywcds_amount"><?php _e( 'Donation for'.' '.$project,'ywcds' );?></label>
+            <label for="ywcds_amount"><?php echo $project;?></label>
             <input type="text" class="ywcds_amount" name="ywcds_amount"/>
         </div>
         <div class="ywcds_button_field">
             <input type="hidden" class="ywcds_product_id" name="add_donation_to_cart" value="<?php echo $product_id;?>" />
-            <input type="submit" name="ywcds_submit" class="ywcds_submit <?php echo $button_class;?>" value="<?php _e( 'Add donation', 'ywcds' );?>" />
+            <input type="submit" name="ywcds_submit_widget" class="ywcds_submit_widget <?php echo $button_class;?>" value="<?php _e( 'Add donation', 'ywcds' );?>" />
         </div>
     </form>
     <img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ) ?>" class="ajax-loading" alt="loading" width="16" height="16" style="visibility:hidden" />
